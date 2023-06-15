60b17f1495403e1de28298a60be9334054358a1b
Marvellous Idoko <idokomarvelous@gmail.com>
2023-06-15 09:29:54 +0100

    June 15, 2023

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,6 @@ export class HomeComponent implements OnInit{
   constructor(private r:Router, private s:SrvService){}
   prods:any[] = []
   ngOnInit(): void {
-    window.addEventListener("beforeunload", function (e) {
-      var confirmationMessage = "\o/";
-    
-      (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-      return confirmationMessage;                            //Webkit, Safari, Chrome
-    });
     this.s.getAllProds().
       subscribe((r: any) => {
         this.prods = r['msg']
--- src/app/shop-x/shop-x.component.ts
+++ src/app/shop-x/shop-x.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { SrvService } from '../srv.service';
 @Component({
   selector: 'app-shop-x',
   templateUrl: './shop-x.component.html',
-  styleUrls: ['./shop-x.component.css']
+  styleUrls: ['./shop-x.component.css', "../home/home.component.css"]
 })
 export class ShopXComponent {
-
+constructor(private r:Router, private s:SrvService){}
+prods:any[] = []
+ngOnInit(): void {
+  this.s.getAllProds().
+    subscribe((r: any) => {
+      this.prods = r['msg']
+    })
+}
+gotDet(prodId:string){
+  this.r.navigateByUrl('product-details/'+ prodId)
+}
 }
